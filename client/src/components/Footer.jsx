93480ad06ebe1149fadf7cbfa4a3b19425a28bbf
Lavit Saini <lavitsaini04@gmail.com>
2025-03-07 02:20:01 +0530

    Footer Updated!

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="border-t-[1px] bg-primary-50">
       <div className="w-full max-w-[72rem] py-5 px-6 mx-auto flex flex-col gap-3 justify-between items-center lg:px-10 sm:flex-row">
         <img src="../assets/images/footer_logo.png" alt="Footer Logo Icon" className="w-40" />
-        <p className="text-neutral-700">2025 Evently. All Rights Reserved</p>
+        <p className="text-neutral-700">2025 <strong className="text-primary-500">Campus Connect</strong>. All Rights Reserved</p>
       </div>
     </footer>
   );
